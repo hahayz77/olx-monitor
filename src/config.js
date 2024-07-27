bb167527b7d25a04b0f223d75734cf48298bd73e
Fialho <danilo__danilo@hotmail.com>
2024-07-27 19:46:15 -0300

    Production ready

---
+++ src/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+let config = {}
+
+config.urls = [
+    process.env.URL1,
+]
+
+// this tool can help you create the interval string:
+// https://tool.crontap.com/cronjob-debugger
+
+config.interval = process.env.CRON_TIME
+config.telegramChatID = process.env.TELEGRAM_CHAT_ID
+config.telegramToken = process.env.TELEGRAM_TOKEN
+config.dbFile = '../data/ads.db'
+
+config.logger={
+    logFilePath: '../data/scrapper.log',
+    timestampFormat:'YYYY-MM-DD HH:mm:ss'
+}
+
+module.exports = config
